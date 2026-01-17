2f75fe852c87c23963ed757402974dfa040c0d1f
Jguardias <camilomartinezg19@gmail.com>
2026-01-17 16:39:38 -0500

    chore: set up database connection environment and gitignore security

---
+++ config/db_init.php
@@ -0,0 +1,20 @@
+
+<?php
+
+include_once('db_datos.php');
+include_once('db_class_meekro.php');
+
+DB::$user = $datos['user'];
+DB::$password = $datos['pass'];
+DB::$dbName = $datos['db'];
+DB::$host = $datos['host'];
+
+//  test connection
+// try {
+
+//     $resultado = DB::query("SELECT 1");
+//     echo "connection successful ";
+// } catch (MeekroDBException $e) {
+//     echo "Error connection: " . $e->getMessage();
+// }
+?>
